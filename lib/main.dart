41e9cce5fe72d014922fb49475d7bbfec600aac2
Mohammad Muwahed <103749096+MohammedMuwahed@users.noreply.github.com>
2026-02-03 05:45:20 +0300

    removing dark mode

--- lib/main.dart
+++ lib/main.dart
@@ -33,7 +33,6 @@ class SmartSleepApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Smart Sleep Control',
-      theme: AppTheme.darkTheme,
       debugShowCheckedModeBanner: false,
       home: const AuthGate(), // 👈 This decides the start page automatically
     );
