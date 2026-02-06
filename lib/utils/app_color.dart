41f224e79e63eb327ee3190c472df80baafb853c
Mohammad Muwahed <103749096+MohammedMuwahed@users.noreply.github.com>
2026-02-06 18:56:36 +0300

    Revert "api"

---
+++ lib/utils/app_color.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Colors.teal;
+  static const Color accent = Colors.tealAccent;
+  static const Color background = Color(0xFF101010);
+}
